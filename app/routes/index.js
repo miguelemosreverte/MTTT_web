29167b060c25c4dbcd5f9b3549e18a23253f362a
miguelemosreverte <miguellemos95@gmail.com>
2017-03-18 02:41:43 -0300

    Added temporary credits for the cloned repository

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', credit: 'https://github.com/b00giZm/docker-compose-nodejs-examples' });
 });
 
 module.exports = router;
