c969834e595ff8bdc3862f2e58d4b271801319d6
miguelemosreverte <miguellemos95@gmail.com>
2017-03-18 14:58:57 -0300

    Now the form submits as a POST, and Express sends the JSON back which is then rendered as text in a div onthe bottom of the form.

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express', credit: 'https://github.com/b00giZm/docker-compose-nodejs-examples' });
 });
 
+router.post('/', function(req, res) {
+
+
+    console.log(req.body);
+    //res.json({ src_lang: req.body.src_lang,trg_lang: req.body.trg_lang,TM: req.body.TM,LM: req.body.LM});    
+    res.json(req.body);
+});
+
 module.exports = router;
