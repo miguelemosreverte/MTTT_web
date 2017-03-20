964dbcc1cf8ae934d985c81fffdb9858018a58d2
miguelemosreverte <miguellemos95@gmail.com>
2017-03-20 01:13:49 -0300

    Extended the limit allowed to be used by the requests, up to 50 mb. This could be exploited in DDoS attacks, and in practice the users would sometimes need even bigger sizes when uploading ther Language Models. But for now it serves as a placeholder.

--- app/app.js
+++ app/app.js
@@ -13,12 +13,11 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false, limit: '50mb'}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
