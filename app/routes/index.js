d1ab78987831147edb4da39687d0f695fe5ce2e6
miguelemosreverte <miguellemos95@gmail.com>
2017-05-18 14:09:34 -0300

    Improved PostEditing and Evaluation tabs, and fixed the utf-8 encoding problem.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var querystring = require('querystring');
 var http = require('http');
 
 /* GET home page. */
@@ -9,14 +10,7 @@ router.get('/', function(req, res) {
 });
 router.post('/CorpusPreparation', function(req, res) {
 
-    //var data = querystring.stringify(req.body);
-    var data = "source_lang="+req.body.src_lang
-                +"&target_lang="+req.body.trg_lang
-                +"&LM_name="+req.body.LM_name
-                +"&LM="+req.body.LM
-                +"&TM_source="+req.body.TM_source
-                +"&TM_target="+req.body.TM_target
-
+    var data = querystring.stringify(req.body);
     var options = {
         host: '172.20.129.2',
         port: 5000,
@@ -45,8 +39,54 @@ router.post('/CorpusPreparation', function(req, res) {
     moses_api_request.end();
 });
 
+
+router.post('/Evaluation', function(req, res) {
+
+    var data = querystring.stringify(req.body);
+    var options = {
+        host: '172.20.129.2',
+        port: 5000,
+        path: '/Evaluate',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': Buffer.byteLength(data)
+        }
+    };
+    var final_result = '';
+    var moses_api_request = http.request(options, function(moses_response) {
+        moses_response.setEncoding('utf8');
+        moses_response.on('data', function (chunk) {
+            final_result += chunk;
+        });
+        moses_response.on('end', function () {
+              res.json(final_result);
+        });
+    });
+    moses_api_request.on('error', function(err) {
+            //res.send('error: ' + err.message);
+            console.log('error: ' + err.message);
+        });
+    moses_api_request.write(data);
+    moses_api_request.end();
+});
+
+router.post('/SetLM', function(req, res) {
+
+    request({
+      uri: "http://172.20.129.2:5000/SetLM/" + req.body['LM_name'],
+      method: "GET",
+    }, function(error, response, body) {
+      if (error){
+        res.json(JSON.stringify(error));
+      }
+      else{
+        res.json(body);
+      }
+    });
+});
+
 router.post('/Translate', function(req, res) {
-    console.log(req.body);
     request({
       uri: "http://172.20.129.2:5000/Translate/" + req.body['TranslationInput'],
       method: "GET",
@@ -60,6 +100,20 @@ router.post('/Translate', function(req, res) {
     });
 
 
+});
+
+router.post('/GetAvailableLMs', function(req, res) {
+    request({
+      uri: "http://172.20.129.2:5000/GetAllAvailableLanguageModelNames",
+      method: "GET",
+    }, function(error, response, body) {
+      if (error){
+        res.json(JSON.stringify(error));
+      }
+      else{
+        res.json(body);
+      }
+    });
 });
 
 router.post('/Train', function(req, res) {
