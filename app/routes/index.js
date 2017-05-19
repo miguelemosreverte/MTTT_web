0357e167ff77fc9b39a3fa2f93f34c393ee6eb26
miguelemosreverte <miguellemos95@gmail.com>
2017-05-19 10:20:57 -0300

    MAJOR refactoring

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,136 +1,57 @@
 var express = require('express');
-var router = express.Router();
 var request = require('request');
-var querystring = require('querystring');
-var http = require('http');
+var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res) {
   res.render('home');
 });
-router.post('/CorpusPreparation', function(req, res) {
 
-    var data = querystring.stringify(req.body);
-    var options = {
-        host: '172.20.129.2',
-        port: 5000,
-        path: '/PrepareCorpus',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Content-Length': Buffer.byteLength(data)
-        }
-    };
-    var final_result = '';
-    var moses_api_request = http.request(options, function(moses_response) {
-        moses_response.setEncoding('utf8');
-        moses_response.on('data', function (chunk) {
-            final_result += chunk;
-        });
-        moses_response.on('end', function () {
-              res.json(final_result);
-        });
-    });
-    moses_api_request.on('error', function(err) {
-            //res.send('error: ' + err.message);
-            console.log('error: ' + err.message);
-        });
-    moses_api_request.write(data);
-    moses_api_request.end();
+router.post('/CorpusPreparation', function(req, res) {
+    request.post('http://moses_api:5000/PrepareCorpus').form({
+        TM_source : req.body.TM_source,
+        TM_target : req.body.TM_target,
+        LM : req.body.LM,
+        source_lang : req.body.source_lang,
+        target_lang : req.body.target_lang,
+        LM_name : req.body.LM_name})
+    .pipe(res);
 });
 
 
 router.post('/Evaluation', function(req, res) {
+  request.post('http://moses_api:5000/Evaluate').form({
+      WER : req.body.WER,
+      PER : req.body.PER,
+      HTER : req.body.HTER,
+      BLEU : req.body.BLEU,
+      BLEU2GRAM : req.body.BLEU2GRAM,
+      BLEU3GRAM : req.body.BLEU3GRAM,
+      BLEU4GRAM : req.body.BLEU4GRAM,
+      EditedMT : req.body.EditedMT,
+      UneditedMT : req.body.UneditedMT})
+  .pipe(res);
 
-    var data = querystring.stringify(req.body);
-    var options = {
-        host: '172.20.129.2',
-        port: 5000,
-        path: '/Evaluate',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Content-Length': Buffer.byteLength(data)
-        }
-    };
-    var final_result = '';
-    var moses_api_request = http.request(options, function(moses_response) {
-        moses_response.setEncoding('utf8');
-        moses_response.on('data', function (chunk) {
-            final_result += chunk;
-        });
-        moses_response.on('end', function () {
-              res.json(final_result);
-        });
-    });
-    moses_api_request.on('error', function(err) {
-            //res.send('error: ' + err.message);
-            console.log('error: ' + err.message);
-        });
-    moses_api_request.write(data);
-    moses_api_request.end();
 });
 
 router.post('/SetLM', function(req, res) {
+      request.get('http://moses_api:5000/SetLM/'+ req.body['LM_name']).pipe(res)
+});
 
-    request({
-      uri: "http://172.20.129.2:5000/SetLM/" + req.body['LM_name'],
-      method: "GET",
-    }, function(error, response, body) {
-      if (error){
-        res.json(JSON.stringify(error));
-      }
-      else{
-        res.json(body);
-      }
-    });
+
+router.get('/GetLM', function(req, res,next) {
+      req.pipe(request.get('http://moses_api:5000/GetLM/'+ req.query.LM_name)).pipe(res)
 });
 
 router.post('/Translate', function(req, res) {
-    request({
-      uri: "http://172.20.129.2:5000/Translate/" + req.body['TranslationInput'],
-      method: "GET",
-    }, function(error, response, body) {
-      if (error){
-        res.json(JSON.stringify(error));
-      }
-      else{
-        res.json(body);
-      }
-    });
-
-
+    request.get('http://moses_api:5000/Translate/'+  req.body['TranslationInput']).pipe(res)
 });
 
 router.post('/GetAvailableLMs', function(req, res) {
-    request({
-      uri: "http://172.20.129.2:5000/GetAllAvailableLanguageModelNames",
-      method: "GET",
-    }, function(error, response, body) {
-      if (error){
-        res.json(JSON.stringify(error));
-      }
-      else{
-        res.json(body);
-      }
-    });
+      request.get('http://moses_api:5000/GetAllAvailableLanguageModelNames').pipe(res)
 });
 
 router.post('/Train', function(req, res) {
-    console.log(req.body);
-    request({
-      uri: "http://172.20.129.2:5000/Train",
-      method: "GET",
-    }, function(error, response, body) {
-      if (error){
-        res.json(JSON.stringify(error));
-      }
-      else{
-        res.json(body);
-      }
-    });
-
-
+    request.get('http://moses_api:5000/Train').pipe(res)
 });
 
 module.exports = router;
