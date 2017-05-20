55e85c01dcd190585740bd5a768b15701f04b95d
miguelemosreverte <miguellemos95@gmail.com>
2017-05-20 19:36:01 -0300

    Now the available languages at the menu on Corpus Preparation are dynamic: They are asked to Moses-API. And TTT used a persistent folder to save the created language models.

--- app/routes/index.js
+++ app/routes/index.js
@@ -9,9 +9,9 @@ router.get('/', function(req, res) {
 });
 
 router.post('/CorpusPreparation', function(req, res) {
-    req.session.LM_name = req.body.LM_name
-    req.session.source_lang = req.body.source_lang
-    req.session.target_lang = req.body.target_lang
+    req.session.LM_name = req.body.LM_name;
+    req.session.source_lang = req.body.source_lang;
+    req.session.target_lang = req.body.target_lang;
     request.post('http://moses_api:5000/PrepareCorpus').form({
         TM_source : req.body.TM_source,
         TM_target : req.body.TM_target,
@@ -42,9 +42,9 @@ router.post('/SetLM', function(req, res) {
       req.session.LM_name = req.body['LM_name']
 });
 
- -router.get('/GetLM', function(req, res,next) {
+router.get('/GetLM', function(req, res,next) {
       req.pipe(request.get('http://moses_api:5000/GetLM/'+ req.query.LM_name)).pipe(res)
-  });
+});
 
 router.post('/Translate', function(req, res) {
     request.post('http://moses_api:5000/Translate').form({
@@ -53,8 +53,13 @@ router.post('/Translate', function(req, res) {
     .pipe(res);
 });
 
-router.post('/GetAvailableLMs', function(req, res) {
+router.get('/GetAvailableLMs', function(req, res) {
       request.get('http://moses_api:5000/GetAllAvailableLanguageModelNames').pipe(res)
+});
+
+
+router.get('/GetAvailableLanguages', function(req, res) {
+      request.get('http://moses_api:5000/GetAvailableLanguages').pipe(res)
 });
 
 router.post('/Train', function(req, res) {
