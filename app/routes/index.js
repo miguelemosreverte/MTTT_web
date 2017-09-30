1fbf4e1273266026207a698b21b54ad882ed8718
miguelemosreverte <miguellemos95@gmail.com>
2017-09-30 20:25:14 -0300

    Since nodemon has a bug node is going to be used to launch the web application. And given that when using docker swarm the docker DNS seems to malfunction, the IP direction 0.0.0.0 is going to temporary replace the DNS direction moses_api where its been used.

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var request = require('request');
 var expressSession = require('express-session');
 var router = express.Router();
-router.use(expressSession({secret:'somesecrettokenhere'}));
+router.use(expressSession({secret:'somesecrettokenhere', resave: false, saveUninitialized: true}));
 
 router.get('/', function(req, res) {
   res.render('home')
@@ -12,7 +12,7 @@ router.post('/CorpusPreparation', function(req, res) {
     req.session.LM_name = req.body.LM_name;
     req.session.source_lang = req.body.source_lang;
     req.session.target_lang = req.body.target_lang;
-    request.post('http://moses_api:5000/PrepareCorpus').form({
+    request.post('http://0.0.0.0:5000/PrepareCorpus').form({
         TM_source : req.body.TM_source,
         TM_target : req.body.TM_target,
         LM : req.body.LM,
@@ -24,7 +24,7 @@ router.post('/CorpusPreparation', function(req, res) {
 
 
 router.post('/Evaluation', function(req, res) {
-  request.post('http://moses_api:5000/Evaluate').form({
+  request.post('http://0.0.0.0:5000/Evaluate').form({
       WER : req.body.WER,
       PER : req.body.PER,
       HTER : req.body.HTER,
@@ -43,27 +43,27 @@ router.post('/SetLM', function(req, res) {
 });
 
 router.get('/GetLM', function(req, res,next) {
-      req.pipe(request.get('http://moses_api:5000/GetLM/'+ req.query.LM_name)).pipe(res)
+      req.pipe(request.get('http://0.0.0.0:5000/GetLM/'+ req.query.LM_name)).pipe(res)
 });
 
 router.post('/Translate', function(req, res) {
-    request.post('http://moses_api:5000/Translate').form({
+    request.post('http://0.0.0.0:5000/Translate').form({
         LM_name : req.body['LM_name'],
         text : req.body['TranslationInput']})
     .pipe(res);
 });
 
 router.get('/GetAvailableLMs', function(req, res) {
-      request.get('http://moses_api:5000/GetAllAvailableLanguageModelNames').pipe(res)
+      request.get('http://0.0.0.0:5000/GetAllAvailableLanguageModelNames').pipe(res)
 });
 
 
 router.get('/GetAvailableLanguages', function(req, res) {
-      request.get('http://moses_api:5000/GetAvailableLanguages').pipe(res)
+      request.get('http://0.0.0.0:5000/GetAvailableLanguages').pipe(res)
 });
 
 router.post('/Train', function(req, res) {
-    request.get('http://moses_api:5000/Train/' + req.session.LM_name + '/' + req.session.source_lang +'/' + req.session.target_lang
+    request.get('http://0.0.0.0:5000/Train/' + req.session.LM_name + '/' + req.session.source_lang +'/' + req.session.target_lang
     ).pipe(res)
 });
 
